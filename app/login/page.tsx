26c10a8075213740cfbbb429f075376b55df01cf
sooinice <sooin9674@naver.com>
2025-04-10 17:26:40 +0900

    feat: 로그인 상태관리

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import MainButton from "../../components/MainButton";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("아이디:", id);
+    console.log("비밀번호:", password);
+  };
+
   return (
     <div className="font-nanum flex flex-col items-center justify-center min-h-screen w-full leading-normal text-text-brown">
       <div className="bg-main-color text-[39.3px] font-extrabold px-[120px] py-[54px]">
@@ -11,22 +22,32 @@ export default function LoginPage() {
         동화 속 이야기의 주인공이 되어보세요!
       </p>
 
-      <form className="flex flex-col items-center mb-[162px]">
+      <form
+        className="flex flex-col items-center mb-[162px]"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin();
+        }}
+      >
         <input
           type="text"
           placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
           className="w-[608px] h-[77px] pl-[34.3px] pt-[25.86px] pr-[473.7px] pb-[19.14px] bg-sub-color text-[20px] outline-none placeholder:text-text-brown placeholder:opacity-[0.45] rounded-tl-[30px] rounded-tr-[30px]"
         />
         <div className="w-[608px] h-[1.5px] bg-[rgba(108,52,1,0.25)]" />
         <input
           type="password"
           placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="w-[608px] h-[77px] pl-[34.3px] pt-[25.86px] pr-[473.7px] pb-[19.14px] bg-sub-color  text-[20px] outline-none placeholder:text-text-brown placeholder:opacity-[0.45] rounded-bl-[30px] rounded-br-[30px]"
         />
+        <div className="mt-[162px]">
+          <MainButton type="submit">로그인</MainButton>
+        </div>
       </form>
-      <Link href="/login">
-        <MainButton type="submit">로그인</MainButton>
-      </Link>
       <div className="flex mt-[36.37px] text-[32px] leading-normal tracking-[-0.96px] gap-[28px]">
         <p className="font-normal">회원이 아니신가요?</p>
         <Link href="/signup" className="font-bold">
