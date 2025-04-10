ed685082364bd12afa382c5feb6564c946e51bc2
sooinice <sooin9674@naver.com>
2025-04-10 15:21:56 +0900

    chore: 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        nanum: ['"NanumSquareRound"', "sans-serif"],
+      },
       colors: {
         "main-color": "#FFBF01",
         "text-brown": "#6C3401",
