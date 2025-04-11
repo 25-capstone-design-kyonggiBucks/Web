76a6ca2f0dcd5e1b12becd6da15b36149c8ebe87
sooinice <sooin9674@naver.com>
2025-04-11 23:47:26 +0900

    feat: 동화 생성 로딩 화면 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,14 @@ module.exports = {
           "0%, 100%": { transform: "translateY(-30%)" },
           "50%": { transform: "translateY(30%)" },
         },
+        "bounce-motion": {
+          "0%, 100%": { transform: "translateY(-10%)" },
+          "50%": { transform: "translateY(10%)" },
+        },
       },
       animation: {
         "bounce-bar": "bounce-bar 1.8s ease-in-out infinite",
+        "bounce-motion": "bounce-motion 1.8s ease-in-out infinite",
       },
     },
   },
