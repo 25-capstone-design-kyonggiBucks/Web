32386e8894cd5f8c31b16dec35ff014723a50992
sooinice <sooin9674@naver.com>
2025-04-11 16:21:10 +0900

    fix: 회원가입 컴포넌트 경로 수정

--- app/(no-navbar)/signup/page.tsx
+++ app/(no-navbar)/signup/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import MainButton from "../../components/MainButton";
+import MainButton from "../../../components/MainButton";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
